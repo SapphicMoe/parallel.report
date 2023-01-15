eb9c3ea3be916f4f2a1faab4363df52d61b66707
Chloe <solely@riseup.net>
2023-01-16 01:33:11 +0500

    refactor: add types, switch from fs to glob

---
+++ src/types.d.ts
@@ -0,0 +1,36 @@
+/**
+ * @file Types
+ * @description Typings for 3rd party resources & contributors
+ * @typedef types
+ */
+
+/**
+ * 3rd party resource
+ */
+export interface Resources {
+  title: string;
+  prefix: string;
+  image: {
+    file: string;
+    height: number;
+    width: number;
+  };
+  description: string;
+  site: string;
+  contributors: {
+    name: string;
+    link: string;
+    role: string;
+  }[];
+}
+
+/**
+ * Contributor(s) for a 3rd-party resource
+ */
+
+export interface Contributor {
+  bold?: Boolean;
+  link: string;
+  name: string;
+  role: string;
+}
