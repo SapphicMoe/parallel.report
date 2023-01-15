dede55d2caed9cf680f33edd142437eb312051de
Chloe <solely@riseup.net>
2023-01-15 16:34:09 +0500

    refactor: file system folder change

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,19 @@ import prefetch from '@astrojs/prefetch';
 import sitemap from '@astrojs/sitemap';
 import compress from 'astro-compress';
 
+// Rehype modules
+import externalLinks from 'rehype-external-links';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.parallel.report',
-  integrations: [mdx(), prefetch(), sitemap(), compress()],
+  integrations: [
+    mdx({
+      extendPlugins: true,
+      rehypePlugins: [externalLinks, { target: '_blank', rel: ['nofollow', 'noopener'] }],
+    }),
+    prefetch(),
+    sitemap(),
+    compress(),
+  ],
 });
