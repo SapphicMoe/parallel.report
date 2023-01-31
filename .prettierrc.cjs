85aa83a0646c6369afa8b2083f9be624fdbc505b
Chloe <solely@riseup.net>
2023-01-31 22:25:04 +0500

    feat: eslint support

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,13 +1,6 @@
 /** @type import('prettier').Config */
 module.exports = {
   plugins: [require.resolve('prettier-plugin-astro')],
-  proseWrap: 'always',
-  printWidth: 120,
-  quoteProps: 'preserve',
-  semi: true,
-  singleQuote: true,
-  tabWidth: 2,
-  trailingComma: 'es5',
   overrides: [
     {
       files: '*.astro',
@@ -16,4 +9,11 @@ module.exports = {
       },
     },
   ],
+  proseWrap: 'always',
+  printWidth: 120,
+  quoteProps: 'preserve',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
 };
