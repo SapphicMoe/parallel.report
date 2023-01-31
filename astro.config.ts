45a67f1c8af72356c7ede8cb21fe2279d71991c0
Chloe <solely@riseup.net>
2023-01-31 22:40:57 +0500

    fix: postcss

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 
-// Astro modules
 import mdx from '@astrojs/mdx';
 import prefetch from '@astrojs/prefetch';
 import sitemap from '@astrojs/sitemap';
 import compress from 'astro-compress';
 
-// Rehype modules
 import externalLinks from 'rehype-external-links';
 
 // https://astro.build/config
