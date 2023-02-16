4e8f08a9cfeb3f5caf6db74f9149c66b7cfd9c0e
Chloe <solely@riseup.net>
2023-02-16 14:50:16 +0500

    fix: markdown links

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,md,mdx,ts}'],
-  plugins: [
-    require('@catppuccin/tailwindcss')({
-      prefix: 'ctp',
-      defaultFlavour: 'mocha',
-    }),
-    require('@tailwindcss/typography'),
-  ],
   theme: {
     fontFamily: {
       body: ['Inter', 'sans-serif'],
@@ -15,14 +8,32 @@ module.exports = {
     },
     colors: {},
     extend: {
-      fontSize: {
-        '3xl': [
-          '1.8rem',
-          {
-            lineHeight: '1.5',
+      typography: () => ({
+        invert: {
+          css: {
+            a: {
+              background: '#f5c2e7',
+              backgroundClip: 'text',
+              color: 'transparent',
+            },
+            'a:hover': {
+              background: '#c99fbe',
+              backgroundClip: 'text',
+              color: 'transparent',
+            },
           },
-        ],
+        },
+      }),
+      fontSize: {
+        '3xl': ['1.8rem', { lineHeight: '1.5' }],
       },
     },
   },
+  plugins: [
+    require('@catppuccin/tailwindcss')({
+      prefix: 'ctp',
+      defaultFlavour: 'mocha',
+    }),
+    require('@tailwindcss/typography'),
+  ],
 };
