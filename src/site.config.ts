409ac906cba6c0037f9f0e0f3061e1ae2cb53771
Chloe <solely@riseup.net>
2023-03-27 22:34:41 +0500

    refactor: clean up the site configuration file

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,46 +1,26 @@
-// Base configuration for this website.
-export const base = {
-  // The favicon for this website
-  favicon: {
-    // Alt text for the favicon
-    alt: 'The Parallel Program emblem icon.',
-    // File name for the favicon (for example, /favicon.png)
-    fileName: '/favicon.png',
+const config = {
+  site: {
+    name: 'parallel.report', // The site name (example.com)
+
+    icon: {
+      name: '/favicon.png', // Icon name (/favicon.png)
+      altText: 'The Parallel Program emblem icon.', // Alt text for the icon
+    },
+
+    themeColor: '#CA2917', // Theme color for the site
+
+    analytics: true, // Whether to enable Plausible Analytics or not
   },
-  // Plausible Analytics settings
-  plausible: {
-    // Whether to enable Plausible Analytics.
-    // If you're using Vercel, you must also edit the rewrites found in the vercel.json file.
-    // Read more about Plausible here: <https://plausible.io/>
-    analytics: true,
+
+  socials: {
+    mastodon: {
+      link: 'https://tech.lgbt/@solely',
+      alt: 'solely@tech.lgbt',
+    },
+
+    twitter: '@solelychloe',
   },
-  // SEO information
-  seo: {
-    // The site's accent color that will appear in embeds
-    color: '#CA2917',
-    // Keywords for search engines
-    keywords: [
-      'parallel program emblem',
-      'parallel program destiny 2',
-      'parallel program emblem destiny 2',
-      'destiny 2 parallel program',
-      'destiny 2 parallel program emblem',
-    ],
-  },
-  // The site name. Used for Plausible Analytics (will appear as https://example.com)
-  site: 'parallel.report',
 };
 
-// Social links for SEO information.
-// If you don't want to link anything here, leave this object empty. Don't uncomment or remove this, as you'll face errors!
-export const socials = {
-  // Your Mastodon instance identifier and link
-  mastodon: {
-    // Instance identifier (will appear as name@instance.link)
-    alt: 'solely@tech.lgbt',
-    // Instance link (will appear as https://instance.link/name)
-    link: 'https://tech.lgbt/@solely',
-  },
-  // Your Twitter username (will appear as https://twitter.com/yourusername)
-  twitter: 'solelychloe',
-};
+export const site = config.site;
+export const socials = config.socials;
