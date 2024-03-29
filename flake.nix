42fbf92e2b2e43efba90d555bec65cc9a9b79504
Chloe <solely@riseup.net>
2024-03-29 21:46:30 +0500

    chore: add basic nixos flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+
+{
+  description = "🔱 Tracks Guardians from Destiny 2 who have received the Parallel Program emblem.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    flake-utils,
+    nixpkgs,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs = [
+              nodejs
+              corepack_latest
+            ];
+          };
+      }
+    );
+}
