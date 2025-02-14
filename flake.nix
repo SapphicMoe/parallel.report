caf7b40f1a393528fc07c52b02a686c1f17949c3
Chloe <chloe@sapphic.moe>
2025-02-14 19:24:45 +0500

    refactor(nix): update flake

--- flake.nix
+++ flake.nix
@@ -1,30 +1,27 @@
-
 {
   description = "🔱 Tracks Guardians from Destiny 2 who have received the Parallel Program emblem.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = {
-    flake-utils,
-    nixpkgs,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
+  outputs =
+    { self, nixpkgs }@inputs:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+          system: function nixpkgs.legacyPackages.${system}
+        );
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            nodejs
+            pnpm
+          ];
         };
-      in {
-        devShells.default = with pkgs;
-          mkShell {
-            buildInputs = [
-              nodejs
-              corepack_latest
-            ];
-          };
-      }
-    );
+      });
+    };
 }
