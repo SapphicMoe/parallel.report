1c4c3d64cd0b0468a1eb612361c5cdacfac87fb8
Chloe <chloe@sapphic.moe>
2025-02-20 03:37:05 +0500

    chore: test 2

--- astro.config.ts
+++ astro.config.ts
@@ -10,7 +10,7 @@ import keystatic from '@keystatic/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
   adapter: vercel(),
   site: 'https://parallel.report',
   integrations: [tailwind(), icon(), react(), keystatic()],
