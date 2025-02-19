19d2c9a8fcdb58c7e5f2edbf2135115bd706ade0
Chloe <chloe@sapphic.moe>
2025-02-20 02:39:08 +0500

    chore: fix keystatic not loading sometimes(?)

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
+
 import react from '@astrojs/react';
-import keystatic from '@keystatic/astro';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
+
 import icon from 'astro-icon';
+
+import keystatic from '@keystatic/astro';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,4 +14,10 @@ export default defineConfig({
   adapter: vercel(),
   site: 'https://parallel.report',
   integrations: [tailwind(), icon(), react(), keystatic()],
+
+  vite: {
+    optimizeDeps: {
+      exclude: ['keystatic', '@keystatic/astro'],
+    },
+  },
 });
